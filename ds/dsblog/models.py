bfc1ef5db8089e749c3334ffb696b6c25e160e66
BassHater <seekua@gmail.com>
2020-06-14 15:33:27 +0200

    Req added

--- ds/dsblog/models.py
+++ ds/dsblog/models.py
@@ -31,31 +31,19 @@ class Tags(models.Model):
 
 class Page(models.Model):
     """PageBlog"""
-    PageTitle = models.CharField(max_length=250, unique=true)
-    PageSlug = models.CharField(max_length=120)
-    PageMetaKeywords = models.ForeignKey(max_length=120)
-    PageMetaDescription = models.TextField("Description")
+    PageTitle = models.CharField(max_length=250, null=False)
+    PageMetaKeywords = models.ForeignKey(max_length=120, null=False)
+    PageMetaDescription = models.TextField("Description", null=False)
     FeaturedImage = models.ImageField("FeaturedImage", upload_to="movies/")
-    CreatedOn = models.DateField("Created on", default=datetime.now)
-    LastEdited = models.DateField("Created on", default=datetime.now)
-    country = models.CharField("Страна", max_length=30)
-    directors = models.ManyToManyField(Actor, verbose_name="режиссер", related_name="film_director")
-    actors = models.ManyToManyField(Actor, verbose_name="актеры", related_name="film_actor")
-    genres = models.ManyToManyField(Genre, verbose_name="жанры")
-    world_premiere = models.DateField("Примьера в мире", default=date.today)
-    budget = models.PositiveIntegerField("Бюджет", default=0,
-                                         help_text="указывать сумму в долларах")
-    fees_in_usa = models.PositiveIntegerField(
-        "Сборы в США", default=0, help_text="указывать сумму в долларах"
+    CreatedOn = models.DateField("Created on", default=datetime.now, null=False)
+    LastEdited = models.DateField("Created on", default=datetime.now, null=False)
+    Category = models.ForeignKey(
+        Category, verbose_name="Category", on_delete=models.SET_NULL, null=True
     )
-    fess_in_world = models.PositiveIntegerField(
-        "Сборы в мире", default=0, help_text="указывать сумму в долларах"
-    )
-    category = models.ForeignKey(
-        Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True
-    )
-    url = models.SlugField(max_length=130, unique=True)
-    draft = models.BooleanField("Черновик", default=False)
+    IsDraft = models.BooleanField("Draft", default=False)
+    IsVisible = models.BooleanField("Draft", default=False)
+
+    PageSlug = models.SlugField("Page Slug", max_length=130, unique=True, null=False)
 
     def __str__(self):
         return self.title
@@ -67,11 +55,7 @@ class Page(models.Model):
         return self.reviews_set.filter(parent__isnull=True)
 
     class Meta:
-        verbose_name = "Фильм"
-        verbose_name_plural = "Фильмы"
+        verbose_name = "Page"
+        verbose_name_plural = "Pages"
 
 
-class Page(models.Model):
-    PageTitle = models.CharField(max_length=250, unique=true)
-    PageSlug = models.CharField(max_length=120)
-    PageMetaKeywords = models.ForeignKey(max_length=120)
